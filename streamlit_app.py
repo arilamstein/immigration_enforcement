be12e8f0b530db2d83045c4df38c1e4d117d4da1
Ari Lamstein <arilamstein@gmail.com>
2025-07-02 19:52:42 -0700

    initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,49 @@
+import requests
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+
+@st.cache_data(ttl="1d")
+def get_detention_data():
+    """URLS of interest:
+    1. TRAC Reports homepage: https://tracreports.org/
+    2. TRAC Reports Immigration page: https://tracreports.org/immigration/
+    3. TRAC Reports Immigration Detention Quick Facts page:
+       https://tracreports.org/immigration/quickfacts/detention.html
+    4. The "See More Data" page when you get on "Detention Quick Facts":
+       https://tracreports.org/immigration/detentionstats/pop_agen_table.html
+    5. The JSON which populates the Detention Quick Facts page:
+       https://tracreports.org/immigration/detentionstats/pop_agen_table.json
+
+    This function gets the data from (5), does some light processing, and returns it.
+    """
+    response = requests.get(
+        "https://tracreports.org/immigration/detentionstats/pop_agen_table.json"
+    )
+
+    df = pd.DataFrame(response.json())
+    df.date = pd.to_datetime(df.date)
+
+    return df
+
+df = get_detention_data()
+df_melted = df.melt(
+    id_vars="date",
+    value_vars=["ice_all", "cbp_all", "total_all"],
+    var_name="category",
+    value_name="count",
+)
+
+fig = px.line(
+    df_melted,
+    x="date",
+    y="count",
+    color="category",
+    color_discrete_sequence=px.colors.qualitative.Safe,
+    title="ICE Detainee Counts Over Time",
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.data_editor(df)
