123d566765880b3c3140db705e44a196ec33dee9
Ari Lamstein <arilamstein@gmail.com>
2025-09-30 17:57:47 -0700

    run black.

--- cbp/cbp_functions.py
+++ cbp/cbp_functions.py
@@ -1,30 +1,34 @@
 import pandas as pd
 from datetime import datetime
 
+
 def convert_fiscal_date_to_calendar_date(fiscal_date):
     """
     Convert a federal fiscal date to its corresponding calendar date.
 
-    The U.S. federal fiscal year begins in October. For reporting purposes, 
-    dates in October through December are considered part of the following 
-    fiscal year. This function adjusts those dates by subtracting one year 
+    The U.S. federal fiscal year begins in October. For reporting purposes,
+    dates in October through December are considered part of the following
+    fiscal year. This function adjusts those dates by subtracting one year
     to align them with the calendar year.
     """
     if fiscal_date.month >= 10:
         return datetime(fiscal_date.year - 1, fiscal_date.month, fiscal_date.day)
     else:
         return fiscal_date
-    
+
+
 def get_monthly_region_df():
     """
     Read in the "Monthly Region" sheet from the "CBP Encounters" Spreadsheet.
-    
+
     Note that the dataset lists dates using Fiscal Years, and those must be converted to Calendar Years
     to work properly with standard charting functions and other datasets.
     """
 
     # Read in data
-    df = pd.read_excel("KHSM Encounters (USBP) fy25m11.xlsx", sheet_name="Monthly Region")
+    df = pd.read_excel(
+        "KHSM Encounters (USBP) fy25m11.xlsx", sheet_name="Monthly Region"
+    )
 
     # Rename columns
     df = df.rename(columns={"Fiscal\nYear": "Fiscal_Year"})
@@ -39,6 +43,6 @@ def get_monthly_region_df():
     df["date"] = df.fiscal_date.apply(convert_fiscal_date_to_calendar_date)
 
     # Now subset columns
-    df = df[['date', 'region', 'quantity']]
+    df = df[["date", "region", "quantity"]]
 
     return df
